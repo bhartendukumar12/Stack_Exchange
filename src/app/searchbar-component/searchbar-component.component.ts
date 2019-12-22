5da8e54aaa158a23cdeecd127340d1a30e33225d
Bhartendu Kumar <hellobhartendu@gmail.com>
2019-12-22 18:36:43 +0530

    css changes

--- src/app/searchbar-component/searchbar-component.component.ts
+++ src/app/searchbar-component/searchbar-component.component.ts
@@ -11,10 +11,12 @@ import { NgxSpinnerService } from 'ngx-spinner';
 export class SearchbarComponentComponent implements OnInit {
 
   userInput:any;
-  searchResults:any;
+  searchResults:any=[];
   lastFiveResults:any=[];
   isQuestionsLoaded:boolean=true;
   answerResults:any=[];
+  isSearchResultAvlbl:boolean=true;
+
   constructor(private commonService : commonService ,  private spinnerService: NgxSpinnerService,  ) { }
 
   ngOnInit() {
@@ -23,21 +25,28 @@ export class SearchbarComponentComponent implements OnInit {
 
   getResult(){
     this.spinnerService.show();
-    this.searchResults=null;
+    this.searchResults=[];
     this.isQuestionsLoaded=true;
     this.commonService.getStoQuestions(this.userInput).subscribe(response => {
       this.spinnerService.hide();
       if (response) {
         this.searchResults=response.items
+        if( this.searchResults.length>0){
+          this.isSearchResultAvlbl=true;
+        }else{
+          this.isSearchResultAvlbl=false;
+        }
         this.lastFiveResults.push({title:this.userInput,result: this.searchResults})
         if(this.lastFiveResults.length==5){
           this.lastFiveResults.splice(0,1)
         }
       }else{
+        this.isSearchResultAvlbl=false;
         this.searchResults=[]
       }
     },
       error => {
+        this.isSearchResultAvlbl=false;
         this.spinnerService.hide();
         this.searchResults=[]
         console.log(error)
@@ -48,6 +57,11 @@ export class SearchbarComponentComponent implements OnInit {
     this.isQuestionsLoaded=true;
     this.userInput=element.title
     this.searchResults=element.result
+    if( this.searchResults.length>0){
+      this.isSearchResultAvlbl=true;
+    }else{
+      this.isSearchResultAvlbl=false;
+    }
   }
 
   navigateToAnswer(questionId){
@@ -55,11 +69,15 @@ export class SearchbarComponentComponent implements OnInit {
    this.isQuestionsLoaded=false;
     this.commonService.getStoAnswers(questionId.question_id).subscribe(response => {
       this.spinnerService.hide();
-      console.log("response",response)
       this.answerResults=response.items[0]
-      console.log("answerResults",this.answerResults)
+      if(this.answerResults.answers && this.answerResults.answers.length>0){
+        this.isSearchResultAvlbl=true;
+      }else{
+        this.isSearchResultAvlbl=false;
+      }
     },
       error => {
+        this.isSearchResultAvlbl=false;
         this.spinnerService.hide();
         this.searchResults=[]
         console.log(error)
